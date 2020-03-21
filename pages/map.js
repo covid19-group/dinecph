0a12b0480dc8b42f7673de7254e1f07ef8513db6
Sebastian Winther <sebastianwinther@gmail.com>
2020-03-21 19:17:04 +0100

    maps init

--- pages/map.js
+++ pages/map.js
@@ -1,3 +1,5 @@
+import { GoogleMap, LoadScriptNext, Marker } from '@react-google-maps/api'
+
 import Head from '../components/Head'
 import Nav from '../components/Nav'
 import LoadingSpinner from '../components/LoadingSpinner'
@@ -5,7 +7,27 @@ import LoadingSpinner from '../components/LoadingSpinner'
 export default () => (
   <>
     <Head />
-    <Nav />
-    <main className="px-3 py-16"></main>
+    <div className="h-screen flex flex-col">
+      <Nav />
+      <main className="flex-auto px-3 pb-3">
+        <LoadScriptNext googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
+          <GoogleMap
+            mapContainerClassName="h-full rounded"
+            zoom={11}
+            center={{
+              lat: 55.6820377,
+              lng: 12.5559868,
+            }}
+          >
+            <Marker
+              position={{
+                lat: 55.6820377,
+                lng: 12.5559868,
+              }}
+            />
+          </GoogleMap>
+        </LoadScriptNext>
+      </main>
+    </div>
   </>
 )
