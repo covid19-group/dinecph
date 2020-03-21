2964a3c907548d0b3c02abd370279fb9c34fe482
Sebastian Winther <sebastianwinther@gmail.com>
2020-03-21 20:03:39 +0100

    fetch dummy-pos

--- components/Map.js
+++ components/Map.js
@@ -1,23 +1,38 @@
+import { useEffect, useState } from 'react'
 import { GoogleMap, LoadScriptNext, Marker } from '@react-google-maps/api'
 
 export default () => {
-  return (
-    <LoadScriptNext googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerClassName="h-full rounded"
-        zoom={12}
-        center={{
-          lat: 55.6820377,
-          lng: 12.5559868,
-        }}
-      >
-        <Marker
-          position={{
-            lat: 55.6820377,
-            lng: 12.5559868,
-          }}
-        />
-      </GoogleMap>
-    </LoadScriptNext>
-  )
+  const [pos, setPos] = useState()
+
+  const getPos = () => {
+    fetch(
+      'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+        'Kleinsgade 6' +
+        '&key=' +
+        process.env.GOOGLE_MAPS_API_KEY
+    )
+      .then(res => res.json())
+      .then(res => {
+        setPos(res.results[0].geometry.location)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  useEffect(() => {
+    getPos()
+  }, [])
+  if (pos)
+    return (
+      <LoadScriptNext googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerClassName="h-full rounded"
+          zoom={12}
+          center={pos}
+        >
+          <Marker position={pos} />
+        </GoogleMap>
+      </LoadScriptNext>
+    )
+  return null
 }
