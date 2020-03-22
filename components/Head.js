a8b3680b0ba30bf13099aea5f6501cd8584bf2e7
Sebastian Winther <sebastianwinther@gmail.com>
2020-03-22 13:23:53 +0100

    favicon + og-image urls

--- components/Head.js
+++ components/Head.js
@@ -7,13 +7,13 @@ export default ({ children }) => {
   return (
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" href="/favicon.png" />
+      <link rel="icon" href="https://dinecph.now.sh/favicon.png" />
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="og:title" content={title} />
       <meta name="og:site_name" content={title} />
       <meta name="og:description" content={description} />
-      <meta name="og:image" content="/og-image.png" />
+      <meta name="og:image" content="https://dinecph.now.sh/og-image.png" />
       <meta name="twitter:card" content="summary" />
       {/* <meta name="twitter:site" content="" /> */}
       <meta name="twitter:title" content={title} />
