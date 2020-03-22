7a33d0e4e325960c228d59202ab89982918314a6
Sebastian Winther <sebastianwinther@gmail.com>
2020-03-22 13:27:32 +0100

    removed non-existant illustrations

--- pages/about.js
+++ pages/about.js
@@ -10,19 +10,16 @@ export default () => (
     <div className="min-h-screen flex flex-col">
       <Nav />
       <main className="flex-auto px-3 pt-16 pb-24">
-        <div className="max-w-6xl flex mx-auto">
-          <div className="flex-auto md:pt-16 md:pr-16">
-            <h2 className="font-extrabold text-2xl sm:text-3xl leading-none mb-6">
-              Om os
-            </h2>
-            <p className="max-w-xl text-navy-light text-lg sm:text-xl mb-2">
-              Covid-19 krisen har ramt restaurant-scenen i København. Mange af
-              dem er begyndt at tilbyde take-away som en respons. Vi har lavet
-              den her side for at sprede ordet. Kontakt os på{' '}
-              <a href="mailto:martin@pelion.app">martin@pelion.app</a>.
-            </p>
-          </div>
-          <div className="flex-shrink-0 hidden md:block w-80 lg:w-96 h-80 lg:h-96 bg-sand" />
+        <div className="max-w-6xl mx-auto">
+          <h2 className="font-extrabold text-2xl sm:text-3xl leading-none mb-6">
+            Om os
+          </h2>
+          <p className="max-w-xl text-navy-light text-lg sm:text-xl mb-2">
+            Covid-19 krisen har ramt restaurant-scenen i København. Mange af dem
+            er begyndt at tilbyde take-away som en respons. Vi har lavet den her
+            side for at sprede ordet. Kontakt os på{' '}
+            <a href="mailto:martin@pelion.app">martin@pelion.app</a>.
+          </p>
         </div>
       </main>
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -10,32 +10,29 @@ export default () => (
     <div className="min-h-screen flex flex-col">
       <Nav />
       <main className="flex-auto px-3 pt-16 pb-24">
-        <div className="max-w-6xl flex items-center mx-auto">
-          <div className="flex-auto md:pr-16">
-            <h1 className="font-extrabold text-4xl sm:text-5xl leading-none mb-6">
-              Støt restauranter i København ved at{' '}
-              <br className="hidden sm:inline" />
-              <span className="text-pink">købe take-away</span>
-            </h1>
-            <p className="max-w-xl text-navy-light text-lg sm:text-xl mb-8">
-              Dine lokale spisesteder er ramt af den nuværende krise—men de
-              laver stadig mad! Hjælp dem til at forblive åbne ved at købe
-              take-away fra de bedste restauranter i København.
-            </p>
-            <div className="-m-2">
-              <Link href="/map">
-                <a className="h-12 btn btn-primary inline-flex items-center m-2">
-                  Find restauranter
-                </a>
-              </Link>
-              <Link href="/submit">
-                <a className="h-12 btn btn-secondary inline-flex items-center m-2">
-                  Tilføj din restaurant
-                </a>
-              </Link>
-            </div>
+        <div className="max-w-6xl mx-auto">
+          <h1 className="max-w-xl font-extrabold text-4xl sm:text-5xl leading-none mb-6">
+            Støt restauranter i København ved at{' '}
+            <br className="hidden sm:inline" />
+            <span className="text-pink">købe take-away</span>
+          </h1>
+          <p className="max-w-xl text-navy-light text-lg sm:text-xl mb-8">
+            Dine lokale spisesteder er ramt af den nuværende krise—men de laver
+            stadig mad! Hjælp dem til at forblive åbne ved at købe take-away fra
+            de bedste restauranter i København.
+          </p>
+          <div className="-m-2">
+            <Link href="/map">
+              <a className="h-12 btn btn-primary inline-flex items-center m-2">
+                Find restauranter
+              </a>
+            </Link>
+            <Link href="/submit">
+              <a className="h-12 btn btn-secondary inline-flex items-center m-2">
+                Tilføj din restaurant
+              </a>
+            </Link>
           </div>
-          <div className="flex-shrink-0 hidden md:block w-80 lg:w-96 h-80 lg:h-96 bg-sand" />
         </div>
       </main>
       <Footer />
