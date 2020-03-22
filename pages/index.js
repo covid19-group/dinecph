e9d07d67e640639a2bc66178ace604f9e5e9ba89
Sebastian Winther <sebastianwinther@gmail.com>
2020-03-22 19:04:05 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,9 @@ export default () => (
               Dine lokale spisesteder er ramt af den nuværende krise—men de
               laver stadig mad! Hjælp dem til at forblive åbne ved at købe
               take-away fra de bedste restauranter i København.
+              {
+                // test
+              }
             </p>
             <div className="pr-3 sm:pr-0 sm:-m-2">
               <Link href="/map">
