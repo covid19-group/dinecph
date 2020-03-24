ffe79d966a4587903bef84dd186fe37304508e5c
Sebastian Winther <sebastianwinther@gmail.com>
2020-03-24 11:52:18 +0100

    Revert "removed tracking from master"
    This reverts commit cbda018b3864965413f362c46c6edfd6054ccc22.

--- components/Head.js
+++ components/Head.js
@@ -19,6 +19,23 @@ export default ({ children }) => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content="https://dinecph.dk/og-image.png" />
+      {process.env.NODE_ENV === 'production' && (
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(f, a, t, h, o, m){
+            a[h]=a[h]||function(){
+            (a[h].q=a[h].q||[]).push(arguments)
+            };
+            o=f.createElement('script'),
+            m=f.getElementsByTagName('script')[0];
+            o.async=1; o.src=t; o.id='fathom-script';
+            m.parentNode.insertBefore(o,m)
+            })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+            fathom('set', 'siteId', 'RKISHSLQ');
+            fathom('trackPageview');`,
+          }}
+        ></script>
+      )}
       {children}
     </Head>
   )
--- components/Nav.js
+++ components/Nav.js
@@ -109,7 +109,13 @@ const Dropdown = ({ language }) => {
               >
                 <li className="w-full">
                   <Link href="/map">
-                    <a className="group flex font-medium px-3 py-2 my-2">
+                    <a
+                      onClick={() =>
+                        process.env.NODE_ENV === 'production' &&
+                        fathom('trackGoal', 'KJOAIHB0', 0)
+                      }
+                      className="group flex font-medium px-3 py-2 my-2"
+                    >
                       {content.restaurants[language].map}
                       <span className="flex-auto text-right text-sand-light group-hover:text-navy-light transition-color duration-150 ease-in-out">
                         ⟶
@@ -119,7 +125,13 @@ const Dropdown = ({ language }) => {
                 </li>
                 <li className="w-full">
                   <Link href="/list">
-                    <a className="group flex font-medium px-3 py-2 my-2">
+                    <a
+                      onClick={() =>
+                        process.env.NODE_ENV === 'production' &&
+                        fathom('trackGoal', 'QKE3VOPK', 0)
+                      }
+                      className="group flex font-medium px-3 py-2 my-2"
+                    >
                       {content.restaurants[language].list}
                       <span className="flex-auto text-right text-sand-light group-hover:text-navy-light transition-color duration-150 ease-in-out">
                         ⟶
